78effd4ab79821c6493955797f4f1658e995b7c1
AtharvaGupta01 <56729502+AtharvaGupta01@users.noreply.github.com>
2021-06-07 18:13:29 +0530

    ,

--- frontend/dd/my-app/src/app/app.module.ts
+++ frontend/dd/my-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -9,7 +9,6 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    NgMultiSelectDropDownModule.forRoot(),
     BrowserModule,
     AppRoutingModule
   ],
