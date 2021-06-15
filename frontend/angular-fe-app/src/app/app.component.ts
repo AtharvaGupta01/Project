4a63205cbe110f173e316d956b331f7eb31d2fbc
AtharvaGupta01 <56729502+AtharvaGupta01@users.noreply.github.com>
2021-06-15 19:34:41 +0530

    .

--- frontend/angular-fe-app/src/app/app.component.ts
+++ frontend/angular-fe-app/src/app/app.component.ts
@@ -1,29 +1,3 @@
-//  import { Component, OnInit } from '@angular/core';
-
-//  @Component({
-//    selector: 'app-root',
-//    templateUrl: './app.component.html',
-//    styleUrls: ['./app.component.css']
-//  })
-//  export class AppComponent implements OnInit {
-//    // title = 'First Attempt';
-//    keys = Object.values({a:1,b:2,c:3})
-//    async fetchData(){
-//      var url = 'http://localhost:5000/'
-//      // var url = 'https://api.chucknorris.io/jokes/random'
-//      var response = await fetch(url)
-//      response = await response.json()
-//      console.log(typeof(response), response)
-//      this.keys = Object.values(response)
-//    }
-
-//    ngOnInit(){
-//      this.fetchData()
-//    }
-// }
-
-
-
 import {Component, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs';
@@ -48,14 +22,6 @@ export class AppComponent implements OnInit {
     var response = await fetch(url)
     response = await response.json()
     this.options = Object.values(response)
-    console.log(typeof(this.my_values), this.my_values)
-    // var keys = [];
-    // for (var nu in this.my_values) {
-    //   if (nu.hasOwnProperty(nu)) {
-    //     keys.push(nu);
-    //   }
-    // }
-    // console.log(typeof(keys))
   }
 
 
@@ -76,3 +42,28 @@ export class AppComponent implements OnInit {
     return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
 }
+
+
+//  import { Component, OnInit } from '@angular/core';
+
+//  @Component({
+//    selector: 'app-root',
+//    templateUrl: './app.component.html',
+//    styleUrls: ['./app.component.css']
+//  })
+//  export class AppComponent implements OnInit {
+//    // title = 'First Attempt';
+//    keys = Object.values({a:1,b:2,c:3})
+//    async fetchData(){
+//      var url = 'http://localhost:5000/'
+//      // var url = 'https://api.chucknorris.io/jokes/random'
+//      var response = await fetch(url)
+//      response = await response.json()
+//      console.log(typeof(response), response)
+//      this.keys = Object.values(response)
+//    }
+
+//    ngOnInit(){
+//      this.fetchData()
+//    }
+// }
